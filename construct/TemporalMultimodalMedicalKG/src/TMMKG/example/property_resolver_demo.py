7ef4cc576815a2e26cc6f701baeb05e3276ab885
devil-son <moguizhiz@126.com>
2026-02-06 12:03:56 +0000

    init_entity_type_resolver

--- construct/TemporalMultimodalMedicalKG/src/TMMKG/example/property_resolver_demo.py
+++ construct/TemporalMultimodalMedicalKG/src/TMMKG/example/property_resolver_demo.py
@@ -4,7 +4,10 @@ from dotenv import load_dotenv, find_dotenv
 from qdrant_client import QdrantClient
 
 from TMMKG.services.encoder.registry import get_text_encoder
-from TMMKG.services.entity_type_resolver import EntityTypeResolver
+from TMMKG.services.entity_type_resolver import (
+    EntityTypeResolver,
+    init_entity_type_resolver,
+)
 from TMMKG.vectorstores.base import build_collection_name
 from TMMKG.vectorstores.qdrant import QdrantVectorStore
 
@@ -25,45 +28,13 @@ _ = load_dotenv(find_dotenv())
 # main
 # -----------------------
 def main():
-    logger.info("Starting EntityTypeResolver demo")
 
-    # -----------------------
-    # 1. init encoder
-    # -----------------------
-
-    encoder, embed_dim = get_text_encoder(
-        "Qwen3-Embedding-8B",
-        model_root=os.getenv("LLM_ROOT"),
-    )
-    logger.info(f"TextEncoder initialized (dim={embed_dim})")
-
-    # -----------------------
-    # 2. init vector store
-    # -----------------------
-
-    base_entity_type_aliases_collection = "entity_type_aliases"
-
-    physical_entity_type_aliases_collection = build_collection_name(
-        base_entity_type_aliases_collection, encoder
-    )
-
-    qdrant_client = QdrantClient(url="http://localhost:6333")
-    vector_store = QdrantVectorStore(
-        collection_name=physical_entity_type_aliases_collection,
-        vector_size=embed_dim,
-        client=qdrant_client,
-    )
-    logger.info("QdrantVectorStore initialized")
-
-    # -----------------------
-    # 3. init resolver
-    # -----------------------
-    resolver = EntityTypeResolver(
-        vector_store=vector_store,
-        encoder=encoder,
+    resolver = init_entity_type_resolver(
+        model_name="Qwen3-Embedding-8B",
+        base_collection="entity_type_aliases",
+        qdrant_url="http://localhost:6333",
         score_threshold=0.75,
     )
-    logger.info("EntityTypeResolver initialized")
 
     # -----------------------
     # 4. test input
