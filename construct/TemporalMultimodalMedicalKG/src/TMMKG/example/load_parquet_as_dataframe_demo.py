37db7b9b96168f859662f74b4509c01a73dba42c
devil-son <moguizhiz@126.com>
2026-02-12 09:08:49 +0000

    load_parquet_as_dataframe

---
+++ construct/TemporalMultimodalMedicalKG/src/TMMKG/example/load_parquet_as_dataframe_demo.py
@@ -0,0 +1,8 @@
+from TMMKG.utils.parquet_utils import load_parquet_as_dataframe
+
+
+if __name__ == "__main__":
+
+    load_parquet_as_dataframe(
+        parquet_path="/home/temp/dataset/home_based_user_training_20260123_v2/parquet/result_1.parquet",
+    )
