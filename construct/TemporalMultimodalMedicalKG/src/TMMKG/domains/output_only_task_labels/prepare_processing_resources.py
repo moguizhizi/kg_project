b48e87ff570d541675c86b1f94a98face3807f5f
devil-son <moguizhiz@126.com>
2026-02-13 06:32:48 +0000

    overwrite

--- construct/TemporalMultimodalMedicalKG/src/TMMKG/domains/output_only_task_labels/prepare_processing_resources.py
+++ construct/TemporalMultimodalMedicalKG/src/TMMKG/domains/output_only_task_labels/prepare_processing_resources.py
@@ -7,6 +7,7 @@ def main():
     paths = xlsx_to_parquet_dataset(
         input_path="/home/temp/dataset/output_only_task_labels/output_only_task_labels.xlsx",
         output_dir="/home/temp/dataset/output_only_task_labels/parquet",
+        overwrite=True,
     )
 
     print(paths)
